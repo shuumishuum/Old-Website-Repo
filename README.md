92014de02e87a6c7a64cc87a517ee913638ca122
shuumishuum <yesomeone1@gmail.com>
2024-03-01 20:25:31 -0800

    Fixes and Additions
    Changelog:
    
    -Added edit button and functionality
    -Added svg and svg credit
    -Fixed favicon file syntax lol

--- README.md
+++
@@ -1,5 +0,0 @@
-This is my first ever decently time-invested web dev project. This is a simple task list I made for myself. If you would like to use it for yourself, you can find it at:
-
-https://shuumishuum.github.io
-
-Thanks for checking this out!
--- toDoList.js
+++ toDoList.js
@@ -14,31 +14,60 @@ addEventListener('DOMContentLoaded', function e () {
         const li = document.createElement('li');
         const textInput = document.createElement('span');
         const deleteButton = document.createElement('span');
+        const editButton = document.createElement('img');
         //make list element child of full task list
         list.appendChild(li);
         //make spans children of the list item appended up top
         li.appendChild(textInput);
         li.appendChild(deleteButton);
+        li.appendChild(editButton);
         //give spans classes so they adhere to html and css rules
         textInput.className = 'task';
         deleteButton.className = 'delete';
+        editButton.className = 'svg';
         //changes the text content of the appended children
         textInput.textContent = newTaskInput;
         deleteButton.textContent = 'Delete';
+        //changes added image to be svg
+        editButton.setAttribute('src', 'editButton.svg');
+        //changes svg width and height
+        editButton.setAttribute('width', '20');
+        editButton.setAttribute('height', '20');
         //It's as simple as this line of code to reset the add task input box. for some odd reason, you can't just use the already defined variable 😠
         document.getElementById('addTaskInput').value = '';
     })
 
-    // //function that adds delete function to delete buttons. p.s. this is copied exactly from netninja js dom tutorial 10. for some reason, intellisense didnt recommend
-    // e.target.className so i spent a whole hour and a half in total on this. wouldve been nice to have the suggestion but i used the code verbatim and it worked. i think
-    // this means i need to understand every propery method and everything of js dom manipulation but im sure ill figure this all out in due time. wonder if there's specific
-    // resources online for this
+    /* function that adds delete function to delete buttons. p.s. this is copied exactly from netninja js dom tutorial 10. for some reason, intellisense didnt recommend
+    e.target.className so i spent a whole hour and a half in total on this. wouldve been nice to have the suggestion but i used the code verbatim and it worked. i think
+    this means i need to understand every propery method and everything of js dom manipulation but im sure ill figure this all out in due time. wonder if there's specific
+    resources online for this */
     list.addEventListener('click', function(e){
         if (e.target.className == 'delete') {
+            //creating whole list element to target in relation to clicked element
             const li = e.target.parentElement;
+            //removing list item from whole list
             list.removeChild(li);
         }
     })
+
+    /*function that allows previous tasks to be editable*/
+    list.addEventListener('click', function(e){
+        if (e.target.className == 'svg') {
+            //creating elements to target in relation to svg element
+            const list = e.target.parentElement;
+            const task = list.querySelector('span');
+            //adding contenteditable attribute to task element
+            task.setAttribute('contenteditable', 'plaintext-only')
+        }
+    })
+
+    // list.addEventListener('click', function(e){
+    //     if(e.target.getElementById = 'li') {
+    //         let tasks = document.getElementsByClassName('task');
+    //         Array.from(tasks).forEach(function(span) {
+    //             span.setAttribute('contenteditable', 'false')
+    //         })
+    //     }})
 
     //displays checkboxes on side of delete button
     // let checkBoxes = document.getElementById('checkBoxes');
